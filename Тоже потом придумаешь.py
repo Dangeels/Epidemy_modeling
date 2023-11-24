ecadbc5dbd8fdfcfd3be63f02b4b0c80fa501291
Dangeels <geels.sasha@mail.ru>
2023-11-24 23:37:02 +0300

    1

---
+++ Тоже потом придумаешь.py
@@ -0,0 +1,72 @@
+import matplotlib as plt
+import numpy as np
+import math
+
+class NazvaniePotomPridumau():
+
+    def __init__(self,
+                 country: str = 'Russia',
+                 days_of_simulation: int = 365,
+                 coef_base: float = 0.35,
+                 coef_quarantine: float = 0.135,
+                 death_rate: float = 0.035,
+                 recovery_rate: float = 0.1,
+                 day_quarantine: int = 73,
+                 incubation_period: int = 15
+                 ):
+        self.country = country
+        self.days_of_simulation = days_of_simulation
+        self.coef_base = coef_base
+        self.coef_quarantine = coef_quarantine
+        # death & recovery rate нужно удалить, так как бесполезны и нужно переписать код без ебатни с геттерами
+        self.death_rate = death_rate
+        self.recovery_rate = recovery_rate
+        #######
+        self.day_quarantine = day_quarantine
+        self.incubation_period = incubation_period
+
+
+
+    def get_coef(self, day: int) -> float:
+        return self.coef_base if day < self.day_quarantine else self.coef_quarantine
+
+    def get_folks_per_day(self, lst: list) -> dict:
+
+        return {
+                'max_day': lst.index(max(lst))+1,
+                'max_day_count' : lst[lst.index(max(lst))],
+                'total' : sum(lst)
+                }
+
+    def simulation(self) -> dict:
+        # Первый инфицированный
+        infected = np.random.randint(1, self.incubation_period, 1)
+
+        infected_lst = []
+        new_cases_lst = []
+        recovered_lst = []
+        deaths_lst = []
+
+        for day in np.arange(1,self.days_of_simulation+1):
+            # Проверяем заражённых на предмет появления симптомов
+            new_cases_idx = np.argwhere(infected == day).flatten()
+            # Удаляем заражённых с симптомами из списка инфицированных, способных заражать
+            infected = np.delete(infected, new_cases_idx)
+            # Генерируем новых заражённых в соответствии с распределением Пуассона и добавляем их к имеющимся
+            new_infected_count = np.random.poisson(self.get_coef(day), infected.size).sum()
+            new_infected = np.random.randint(1, self.incubation_period, new_infected_count) + day
+            infected = np.concatenate((infected, new_infected))
+            # Заполняем статистику
+            infected_lst.append(infected.size)
+            new_cases_lst.append(new_cases_idx.size)
+            recovered_lst.append(math.floor(infected.size * self.recovery_rate))
+            deaths_lst.append(math.floor(infected.size * self.death_rate))
+
+        return {'infected':infected_lst,'new_cases':new_cases_lst,'recovered':recovered_lst,'deaths':deaths_lst}
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+
+    suka = NazvaniePotomPridumau()
+    print(suka.get_folks_per_day(suka.simulation()['deaths']))
